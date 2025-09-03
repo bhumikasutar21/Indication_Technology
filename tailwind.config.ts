e1f2046eba2844ef4a58d66407d6066d7134cef4
indication <bhumikasutar21@gmail.com>
2025-09-04 01:54:02 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,11 +38,17 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
+  // content: [
+  //   "./pages/**/*.{ts,tsx}",
+  //   "./components/**/*.{ts,tsx}",
+  //   "./app/**/*.{ts,tsx}",
+  //   "./src/**/*.{ts,tsx}",
+  // ],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./src/**/*.{ts,tsx,js,jsx}",
   ],
   prefix: "",
   theme: {
